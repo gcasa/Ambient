89b27964da8fc19497d177de59e801d29cf0f3a9
Gregory Casamento <greg.casamento@gmail.com>
2026-08-07 07:00:51 -0400

    Update resources

--- Atmosphere/main.m
+++ Atmosphere/main.m
@@ -7,6 +7,27 @@ int main(int argc, const char * argv[]) {
         ATAppDelegate *delegate = [ATAppDelegate new];
         app.delegate = delegate;
         [app setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+        NSMenu *mainMenu = [[NSMenu alloc] initWithTitle:@"Main Menu"];
+        NSMenuItem *applicationMenuItem = [[NSMenuItem alloc] initWithTitle:@"Atmosphere" action:nil keyEquivalent:@""];
+        [mainMenu addItem:applicationMenuItem];
+
+        NSMenu *applicationMenu = [[NSMenu alloc] initWithTitle:@"Atmosphere"];
+        NSMenuItem *aboutItem = [[NSMenuItem alloc] initWithTitle:@"About Atmosphere"
+                                                          action:@selector(orderFrontStandardAboutPanel:)
+                                                   keyEquivalent:@""];
+        aboutItem.target = app;
+        [applicationMenu addItem:aboutItem];
+        [applicationMenu addItem:NSMenuItem.separatorItem];
+
+        NSMenuItem *quitItem = [[NSMenuItem alloc] initWithTitle:@"Quit Atmosphere"
+                                                         action:@selector(terminate:)
+                                                  keyEquivalent:@"q"];
+        quitItem.target = app;
+        [applicationMenu addItem:quitItem];
+        applicationMenuItem.submenu = applicationMenu;
+        app.mainMenu = mainMenu;
+
         [app run];
     }
     return 0;
